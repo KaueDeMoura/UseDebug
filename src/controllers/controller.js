a22ea4ad70f4974b229dd1758765e797f82677b9
KaueDeMoura <kaueoenningdmoura@gmail.com>
2024-03-27 21:58:38 -0300

    testes realizados

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -4,7 +4,7 @@ const servico = new ServicoExercicio()
 
 export default class ControllerExercicio {
 
-    Somar(req, res){
+   /* Somar(req, res){
       try {
         const num1 = req.body.num1--;
         const num2 = req.body.num2;
@@ -16,8 +16,23 @@ export default class ControllerExercicio {
         res.status(500).json({ message: "Erro ao somar"});
       }
     }
+    */
 
-    Subtrair(req, res){
+    Somar(req, res) {
+   
+      try {
+        const num1 = req.body.num1;
+        const num2 = req.body.num2;
+   
+        const result = servico.Somar(num1, num2);
+   
+        res.status(200).json({ resultado: result });
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    }
+
+    /*Subtrair(req, res){
       try {
         const num1 = req.body.num1;
         const num2 = req.body.num2;
@@ -28,9 +43,22 @@ export default class ControllerExercicio {
       } catch (error) {
         res.status(500).json({ message: "Erro ao Subtrair"});
       }
-    }
+    }*/
 
-    Dividir(req, res){
+    Subtrair(req, res) {
+      try {
+          const num1 = req.body.num1;
+          const num2 = req.body.num2;
+
+          const result = servico.Subtrair(num1, num2);
+
+          res.status(200).json({ resultado: result });
+      } catch (error) {
+          res.status(500).json({ message: "Erro ao subtrair" });
+      }
+  }
+
+    /*Dividir(req, res){
       try {
         const num1 = req.body.num1;
         const num2 = req.body.num2;
@@ -42,8 +70,21 @@ export default class ControllerExercicio {
         res.status(500).json({ message: "Erro ao Dividir"});
       }
     }
+*/
 
-    Multiplicar(req, res){
+      Dividir(req, res) {
+        try {
+            const num1 = req.body.num1;
+            const num2 = req.body.num2;
+
+            const result = servico.Dividir(num1, num2);
+
+            res.status(200).json({ resultado: result });
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao dividir" });
+        }
+      }
+    /*Multiplicar(req, res){
       try {
         const num1 = req.body.num1
         const num2 = req.body.num2
@@ -54,6 +95,17 @@ export default class ControllerExercicio {
       } catch (error) {
         res.status(500).json({ message: "Erro ao Multiplicar"});
       }
-    }
+    }*/
+    Multiplicar(req, res) {
+      try {
+          const num1 = req.body.num1;
+          const num2 = req.body.num2;
 
+          const result = servico.Multiplicar(num1, num2);
+
+          res.status(200).json({ resultado: result });
+      } catch (error) {
+          res.status(500).json({ message: "Erro ao multiplicar" });
+      }
+  }
 }
